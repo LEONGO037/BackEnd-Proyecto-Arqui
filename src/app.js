7b8e755981fecddd5c1016196f675a4d92368510
maycro <juan.cordeiro@ucb.edu.bo>
2026-02-24 08:10:25 -0400

    Vistas y Pagos V0.0.2

--- src/app.js
+++ src/app.js
@@ -1,23 +1,19 @@
 import express from "express";
 import cors from "cors";
-import rutasAutenticacion  from "./routes/autenticacion.rutas.js";
-import rutasAdmin          from "./routes/admin.rutas.js";
-import rutasInscripciones  from "./routes/inscripcion/inscripcion.routes.js";
-import rutasCursos         from "./routes/cursos/cursos.routes.js";
-import rutasPagos          from "./routes/pagos/pagos.routes.js";
 import rutasAutenticacion from "./routes/autenticacion.rutas.js";
-import rutasAdmin from "./routes/admin.rutas.js";
-import rutasEstudiante from "./routes/estudiante/estudiante.routes.js";
+import rutasAdmin         from "./routes/admin.rutas.js";
+import rutasEstudiante    from "./routes/estudiante/estudiante.routes.js";
+import rutasInscripciones from "./routes/inscripcion/inscripcion.routes.js";
+import rutasCursos        from "./routes/cursos/cursos.routes.js";
+import rutasPagos         from "./routes/pagos/pagos.routes.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/api/autenticacion", rutasAutenticacion);
-app.use("/api/admin", rutasAdmin);
-app.use("/api/estudiante", rutasEstudiante);
 
 app.use("/api/autenticacion",  rutasAutenticacion);
 app.use("/api/admin",          rutasAdmin);
+app.use("/api/estudiante",     rutasEstudiante);
 app.use("/api/inscripciones",  rutasInscripciones);
 app.use("/api/cursos",         rutasCursos);
 app.use("/api/pagos",          rutasPagos);
